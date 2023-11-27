b4c7bb792fe6ebfdeb9a8813498d860b5dd47158
Emanuel-Thiel <50388234+Emanuel-Thiel@users.noreply.github.com>
2023-11-27 20:37:13 -0300

    Update utils.c
    criadas as funções para printar o labels e as "arestas"

--- utils.c
+++ utils.c
@@ -1,83 +1,217 @@
 #include <string.h>
+
 #include <stdlib.h>
+
 #include <stdio.h>
+
 #include "utils.h"
+
 #define ARQUIVO_SAIDA "saida.dot"
 
+
+
 extern int yylineno;
+
 extern void *arvore;
 
+
+
 int get_line_number() {
+
 	return yylineno;
+
 }
+
+
 
 void set_root(asd_tree_t *t) {
+
   arvore = t;
+
 }
+
+
 
 void yyerror(const char *s) {
+
 	printf("In the line %d, the following error occurred: %s", get_line_number(), s);
+
 }
+
+
 
 asd_tree_t *asd_new(lex_val label)
+
 {
+
   asd_tree_t *ret = NULL;
+
   ret = calloc(1, sizeof(asd_tree_t));
+
   if (ret != NULL){
+
     ret->label.num_line = label.num_line;
+
 	ret->label.token_type = strdup(label.token_type);
+
 	ret->label.token_value = strdup(label.token_value);
+
     ret->number_of_children = 0;
+
     ret->children = NULL;
+
   }
+
   return ret;
+
 }
+
+
 
 void asd_free(asd_tree_t *tree)
+
 {
+
   if (tree != NULL){
+
     int i;
+
     for (i = 0; i < tree->number_of_children; i++){
+
       asd_free(tree->children[i]);
+
     }
+
     free(tree->children);
+
     free(tree->label.token_type);
+
 	free(tree->label.token_value);
+
     free(tree);
+
   }else{
+
     printf("Erro: %s recebeu parâmetro tree = %p.\n", __FUNCTION__, tree);
+
   }
+
 }
+
+
 
 void asd_add_child(asd_tree_t *tree, asd_tree_t *child)
+
 {
+
   if (tree != NULL && child != NULL){
+
     tree->number_of_children++;
+
     tree->children = realloc(tree->children, tree->number_of_children * sizeof(asd_tree_t*));
+
     tree->children[tree->number_of_children-1] = child;
+
   }else{
-    printf("Erro: %s recebeu parâmetro tree = %p / %p.\n", __FUNCTION__, tree, child);
+
+    //printf("Erro: %s recebeu parâmetro tree = %p / %p.\n", __FUNCTION__, tree, child);
+
   }
+
 }
+
+
+
+static void imprimirLabels(asd_tree_t *tree){
+
+	if (tree != NULL){
+
+		printf("%p [label=\"%s\"]\n", (void *) tree, tree->label.token_value);
+
+		for (int i = 0; i < tree->number_of_children; i++){
+
+      			imprimirLabels(tree->children[i]);
+
+    		}
+
+	}
+
+}
+
+
+
+static void imprimirArestas(asd_tree_t *tree){
+
+	for (int i = 0; i < tree->number_of_children; i++){
+
+		if (tree->children[i] != NULL){
+
+			printf("%p, %p\n", (void *) tree, (void *) tree->children[i]);
+
+		}
+
+    	}
+
+    	for (int i = 0; i < tree->number_of_children; i++){
+
+		if (tree->children[i] != NULL){
+
+			imprimirArestas(tree->children[i]);
+
+		}
+
+    	}
+
+}
+
+
 
 static void _asd_print (FILE *foutput, asd_tree_t *tree, int profundidade)
+
 {
+
   int i;
+
   if (tree != NULL){
+
     fprintf(foutput, "%d%*s: Nó '%s' tem %d filhos:\n", profundidade, profundidade*2, "", tree->label.token_value, tree->number_of_children);
+
+    imprimirArestas(tree);
+
     for (i = 0; i < tree->number_of_children; i++){
+
       _asd_print(foutput, tree->children[i], profundidade+1);
+
     }
+
   }else{
-    printf("Erro: %s recebeu parâmetro tree = %p.\n", __FUNCTION__, tree);
+
+    //printf("Erro: %s recebeu parâmetro tree = %p.\n", __FUNCTION__, tree);
+
   }
+
 }
 
+
+
 void exporta (void *arvore) {
+
 	FILE *foutput = stderr;
+
 	if (arvore != NULL){
-		_asd_print(foutput, arvore, 0);
+
+		//_asd_print(foutput, arvore, 0);
+
+		imprimirLabels(arvore);
+
+		printf("\n\n");
+
+		imprimirArestas(arvore);
+
 	} else {
-		printf("Erro: %s recebeu parâmetro tree = %p.\n", __FUNCTION__, arvore);
+
+		//printf("Erro: %s recebeu parâmetro tree = %p.\n", __FUNCTION__, arvore);
+
 	}
+
 }
