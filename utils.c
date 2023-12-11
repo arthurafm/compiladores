e32ae6b26468e5a5fabf26d1f46421eb7cf6425b
Arthur <108934652+arthurafm@users.noreply.github.com>
2023-12-11 14:43:08 -0300

    Iniciada implementação da tabela de símbolos através de hash table
    O seguinte tutorial está sendo seguido: https://www.digitalocean.com/community/tutorials/hash-table-in-c-plus-plus
    
    Documentação adicional do código também foi inserida.

--- utils.c
+++ utils.c
@@ -7,15 +7,17 @@ extern int yylineno;
 extern void *arvore;
 
 int get_line_number() {
-  return yylineno;
-}
-
-void set_root(tree_t *t) {
-  arvore = t;
+    return yylineno;
 }
 
 void yyerror(const char *s) {
-	printf("In the line %d, the following error occurred: %s", get_line_number(), s);
+	  printf("In the line %d, the following error occurred: %s", get_line_number(), s);
+}
+
+/* --- Árvore Sintática Abstrata --- */
+
+void set_root(tree_t *t) {
+  arvore = t;
 }
 
 tree_t *tree_new(lex_val info) {
@@ -85,4 +87,125 @@ void exporta (void *arvore) {
 		printf("\n\n");
 		imprimirArestas(arvore);
 	}
+}
+
+/* --- Tabela de Símbolos --- */
+
+/* Algoritmo djb2, disponível em http://www.cse.yorku.ca/~oz/hash.html */
+unsigned long hash_function (unsigned char *str) {
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c;
+
+    return hash;
+}
+
+ht_item* create_item (char *key, int num_line, char *nature, char *type, char *token_value) {
+
+    ht_item *item = (ht_item *) malloc(sizeof(ht_item));
+    item->key = (char *) malloc(strlen(key) + 1);
+    item->nature = (char *) malloc(strlen(nature) + 1);
+    item->type = (char *) malloc(strlen(type) + 1);
+    item->token_value = (char *) malloc(strlen(token_value) + 1);
+
+    strcpy(item->key, key);
+    item->num_line = num_line;
+    strcpy(item->nature, nature);
+    strcpy(item->type, type);
+    strcpy(item->token_value, token_value);
+
+    return item;
+}
+
+hash_table* create_table (int size) {
+    
+    hash_table *table = (hash_table *) malloc(sizeof(hash_table));
+    table->items = (ht_item **) calloc(size, sizeof(ht_item*));
+    table->size = size;
+    table->count = 0;
+
+    for (int i = 0; i < size; i++) {
+        table->items[i] = NULL;
+    }
+
+    return table;
+}
+
+void free_item (ht_item* item) {
+
+    free(item->key);
+    free(item->nature);
+    free(item->type);
+    free(item->token_value);
+
+    free(item);
+}
+
+void free_table (hash_table *table) {
+
+    for (int i = 0; i < table->size; i++) {
+        ht_item *item = table->items[i];
+        if (item != NULL) {
+            free(item);
+        }
+    }
+
+    free(table->items);
+    free(table);
+}
+
+void ht_insert (hash_table *table, char *key, int num_line, char *nature, char *type, char *token_value) {
+    
+    ht_item *item = create_item(key, num_line, nature, type, token_value);
+    int index = hash_function(key);
+
+    ht_item *cur_item = table->items[index];
+
+    /* Não houve colisão */
+    if (cur_item == NULL) {
+        /* A hash table está cheia */
+        if (table->count == table->size) {
+            printf("Insert error: hash table is full.\n");
+            free(item);
+            return;
+        }
+
+        /* Caso a hash table não esteja cheia */
+        table->items[index] = item;
+        table->count++;
+    }
+    /* Houve colisão */
+    else {
+        /* A mesma key está na tabela */
+        /* TODO: Tratamento especial da E4 --> Caso o identificador já esteja na tabela, é necessário que se retorne um erro */
+        if (strcmp(cur_item->key, key) == 0) {
+            strcpy(table->items[index]->key, key);
+            table->items[index]->num_line = num_line;
+            strcpy(table->items[index]->nature, nature);
+            strcpy(table->items[index]->type, type);
+            strcpy(table->items[index]->token_value, token_value);
+            return;
+        }
+        /* Colisão não é na mesma key */
+        else {
+            handle_collision(table, item);
+            return;
+        }
+    }
+}
+
+/* A decidir o que esta função retornará -> Tipo? Token_value? */
+char* ht_search(hash_table *table, char *key) {
+    int index = hash_function(key);
+    ht_item *item = table->items[index];
+
+    if (item != NULL) {
+        if (strcmp(item->key, key) == 0) {
+            return item->type;
+        }
+    }
+
+    return NULL;
 }
--- utils.h
+++ utils.h
@@ -1,23 +1,57 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
+#define ERR_UNDECLARED  10
+#define ERR_DECLARED    11
+#define ERR_VARIABLE    20
+#define ERR_FUNCTION    21
+
+/* --- Estrutura da AST --- */
+
 typedef struct lex_value {
     int num_line;
     char *token_type;
     char *token_value;
+    char *token_type;
 } lex_val;
 
 typedef struct tree {
-  lex_val info;
-  int number_of_children;
-  struct tree **children;
+    lex_val info;
+    int number_of_children;
+    struct tree **children;
 } tree_t;
 
+/* --- Estrutura da Tabela de Símbolos --- */
+
+typedef struct hash_table_item {
+    char *key;
+    int num_line;
+    char *nature;
+    char *type;
+    char *token_value;
+} ht_item;
+
+typedef struct hashtable {
+    ht_item **items;
+    int size;
+    int count;
+} hash_table;
+
+typedef struct linkedlist {
+    ht_item *item;
+    struct linkedlist* next;
+} linked_list;
+
+/* Retorna o número da linha atual do código-fonte */
 int get_line_number();
 
-void set_root(tree_t *t);
-
+/* Printa uma mensagem de erro */
 void yyerror(const char *s);
+
+/* --- Árvore Sintática Abstrata --- */
+
+/* Seta raiz da árvore */
+void set_root(tree_t *t);
 
 /* Cria uma nova árvore */
 tree_t *tree_new(lex_val info);
@@ -27,5 +61,28 @@ void tree_free(tree_t *tree);
 
 /* Adiciona child como filho de tree */
 void tree_add_child(tree_t *tree, tree_t *child);
+
+/* --- Tabela de Símbolos --- */
+
+/* Função de hash */
+unsigned long hash_function (unsigned char *str);
+
+/* Cria um item na hash table */
+ht_item* create_item (char *key, int num_line, char *nature, char *type, char *token_value);
+
+/* Cria uma hash table */
+hash_table* create_table (int size);
+
+/* Destrói um item de hash table */
+void free_item (ht_item* item)
+
+/* Destrói uma hash table */
+void free_table (hash_table *table);
+
+/* Insere em uma hash table */
+void ht_insert (hash_table *table, char *key, int num_line, char *nature, char *type, char *token_value);
+
+/* Busca em uma hash table */
+char* ht_search(hash_table *table, char *key);
 
 #endif //_UTILS_H_
